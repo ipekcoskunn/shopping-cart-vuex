8e4358f2206629265c3c2c2798a006426bc1a2e1
ipekcoskun <ipekcoskun@omegabigdata.com>
2024-01-11 23:11:18 +0300

    bug fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-// import { createApp } from 'vue'
-import Vue from 'vue';
+import { createApp } from 'vue'
+//import Vue from 'vue';
 import App from './App.vue'
-import { store } from './store'
+//import { store } from './store'
 
-// createApp(App).mount('#app')
+createApp(App).mount('#app')
 
-new Vue({
-    store,
-    render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//     store,
+//     render: h => h(App),
+// }).$mount('#app')
